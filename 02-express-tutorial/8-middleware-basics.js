955e047f9d9c5a2cbf7da73347e195cf94174b28
Duke-Skygawker <bart3kk990@gmail.com>
2021-09-10 12:43:42 +0200

    queries and middlewares

--- 02-express-tutorial/8-middleware-basics.js
+++ 02-express-tutorial/8-middleware-basics.js
@@ -21,3 +21,26 @@ app.get("/about", (req, res) => {
 app.listen(5000, () => {
   console.log("server is listening on port 5000...");
 });
+const express = require("express");
+const app = express();
+
+//req(uest) => middleware => res(ponse)
+
+const logger = (req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  console.log(method, url, time);
+  next();
+};
+
+app.get("/", logger, (req, res) => {
+  res.send("Home");
+});
+app.get("/about", (req, res) => {
+  res.send("About");
+});
+
+app.listen(5000, () => {
+  console.log("server is listening on port 5000...");
+});
